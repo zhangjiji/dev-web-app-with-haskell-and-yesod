a438faed7bae0b8f4199f8ebc42dc6285a1dd06d
zhangjiji <programmer.zhangjiji@gmail.com>
2014-04-24 21:13:49 +0800

    hello world

---
+++ helloworld.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE TypeFamilies, QuasiQuotes, MultiParamTypeClasses, TemplateHaskell, OverloadedStrings #-}
+
+import Yesod
+
+data HelloWorld = HelloWorld
+
+mkYesod "HelloWorld" [parseRoutes|/ HomeR GET|]
+
+instance Yesod HelloWorld
+
+getHomeR :: Handler Html
+getHomeR = defaultLayout [whamlet|Hello World!|]
+
+main :: IO ()
+main = warp 3000 HelloWorld
+

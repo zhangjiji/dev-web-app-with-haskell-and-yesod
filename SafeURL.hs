ff4dbbfa99f318ddd6052531af3ace10bea2c67b
zhangjiji <programmer.zhangjiji@gmail.com>
2014-05-06 16:52:54 +0800

    practice

---
+++ SafeURL.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+import Text.Hamlet (HtmlUrl, hamlet)
+import Text.Blaze.Html.Renderer.String (renderHtml)
+import Data.Text (Text)
+
+data MyRoute = Home
+
+render :: MyRoute -> [(Text, Text)] -> Text
+render Home _ = "/home"
+
+footer :: HtmlUrl MyRoute
+footer = [hamlet|
+          <footer>
+              Return to #
+              <a href=@{Home}>HomePage
+              . 
+                |]
+
+main :: IO ()
+main = putStrLn $ renderHtml $ [hamlet|
+                                <body>
+                                    <p>This is my page.
+                                    ^{footer}
+                                      |] render
